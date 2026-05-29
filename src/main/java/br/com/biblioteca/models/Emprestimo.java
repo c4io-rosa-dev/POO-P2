3073211a87c705c43966f5f27d7187490102f89e
viniciuskenji7 <viniciuskenji007@gmail.com>
2026-05-29 15:37:49 -0300

    feat: models emprestimo e reserva

--- src/main/java/br/com/biblioteca/models/Emprestimo.java
+++ src/main/java/br/com/biblioteca/models/Emprestimo.java
@@ -1,5 +1,65 @@
 package br.com.biblioteca.models;
 
+import java.util.Date;
+
 public class Emprestimo {
+    private int id;
+    private Leitor leitorId;
+    private Copia copiaId;
+    private Funcionario funcionarioId;
+    private Date dataEmprestimo;
+    private Date dataPrevistaDevoulcao;
+    private Date dataDevolucao;
+    private String status;
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataPrevistaDevoulcao() {
+        return dataPrevistaDevoulcao;
+    }
+
+    public void setDataPrevistaDevoulcao(Date dataPrevistaDevoulcao) {
+        this.dataPrevistaDevoulcao = dataPrevistaDevoulcao;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Leitor getLeitorId() {
+        return leitorId;
+    }
+
+    public Copia getCopiaId() {
+        return copiaId;
+    }
+
+    public Funcionario getFuncionarioId() {
+        return funcionarioId;
+    }
+
+        
 
 }
--- src/main/java/br/com/biblioteca/models/Reserva.java
+++ src/main/java/br/com/biblioteca/models/Reserva.java
@@ -1,5 +1,50 @@
 package br.com.biblioteca.models;
 
+import java.util.Date;
+
 public class Reserva {
+    private int id;
+    private Leitor leitorId;
+    private Obra obraId;
+    private Date dataReserva;
+    private Date dataExpiracao;
+    private String status;
+    public Date getDataReserva() {
+        return dataReserva;
+    }
+    public void setDataReserva(Date dataReserva) {
+        this.dataReserva = dataReserva;
+    }
+    public Date getDataExpiracao() {
+        return dataExpiracao;
+    }
+    public void setDataExpiracao(Date dataExpiracao) {
+        this.dataExpiracao = dataExpiracao;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Leitor getLeitorId() {
+        return leitorId;
+    }
+    public void setLeitorId(Leitor leitorId) {
+        this.leitorId = leitorId;
+    }
+    public Obra getObraId() {
+        return obraId;
+    }
+    public void setObraId(Obra obraId) {
+        this.obraId = obraId;
+    }
+
     
 }
