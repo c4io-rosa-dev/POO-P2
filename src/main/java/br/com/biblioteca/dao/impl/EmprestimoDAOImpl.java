970f7e7607ebcd91a5aec5a36286a15557606b90
viniciuskenji7 <viniciuskenji007@gmail.com>
2026-05-29 17:15:08 -0300

    feat: criando os DAOimpl

---
+++ src/main/java/br/com/biblioteca/dao/impl/EmprestimoDAOImpl.java
@@ -0,0 +1,5 @@
+package br.com.biblioteca.dao.impl;
+
+public class EmprestimoDAOImpl {
+
+}
---
+++ src/main/java/br/com/biblioteca/dao/impl/LeitorDAOImpl.java
@@ -0,0 +1,8 @@
+package br.com.biblioteca.dao.impl;
+
+import br.com.biblioteca.dao.LeitorDAO;
+import br.com.biblioteca.models.Leitor;
+
+public class LeitorDAOImpl implements LeitorDAO {
+    
+}
